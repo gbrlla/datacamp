fedadf114507453ff830677b736ff5eed4b803db
gbrlla <gabinp@outlook.com>
2019-03-14 08:50:47 -0300

    teste

---
+++ tidyverse-learning.R
@@ -0,0 +1,19 @@
+install.packages("gapminder")
+install.packages("tidyverse")
+
+
+library(tidyverse)
+library(gapminder)
+library(ggplot2)
+
+#teste
+
+# Summarize medianGdpPercap within each continent within each year: by_year_continent
+by_year_continent <- gapminder %>%
+  group_by(continent, year) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Plot the change in medianGdpPercap in each continent over time
+ggplot(by_year_continent, aes(x = year, y = medianGdpPercap, color = continent)) +
+  geom_point() +
+  expand_limits(y = 0)
