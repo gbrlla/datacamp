7af6ebf7d6f120513a5db96a653998dd7978d905
Gabriella do Nascimento Pinheiro <B248968182@ipea.gov.br>
2019-03-26 18:24:50 -0300

    parte 1

--- Importing_data_in_R.R
+++ Importing_data_in_R.R
@@ -1,7 +1,9 @@
-##### COURSE: Importing data in R
+##### COURSE: Importing data in R (PT1)
 
 
-#####CHAPTER1
+# ------
+
+#CHAPTER1
 
 # Import swimming_pools.csv: pools
 pools <- read.csv("swimming_pools.csv")
@@ -52,7 +54,9 @@ lily
 tom
 
 
-###### CHAPTER2
+# -------- 
+
+# CHAPTER2
 
 # Load the readr package
 library(readr)
@@ -140,4 +144,165 @@ potatoes <- fread(("potatoes.csv"), select = c(6,8))
 plot(potatoes$texture, potatoes$moistness)
 
 
-#####CHAPTER3
+# ---------
+
+# CHAPTER 3
+
+
+
+# Load the readxl package
+library(readxl)
+
+# Print the names of all worksheets
+excel_sheets("urbanpop.xlsx")
+
+# Read the sheets, one by one
+pop_1 <- read_excel("urbanpop.xlsx", sheet = 1)
+pop_2 <- read_excel("urbanpop.xlsx", sheet = 2)
+pop_3 <- read_excel("urbanpop.xlsx", sheet = 3)
+
+# Put pop_1, pop_2 and pop_3 in a list: pop_list
+pop_list <- list(pop_1, pop_2, pop_3)
+
+# Display the structure of pop_list
+str(pop_list)
+
+# Read all Excel sheets with lapply(): pop_list
+pop_list <- lapply(excel_sheets("urbanpop.xlsx"), read_excel, path = "urbanpop.xlsx")
+
+# Display the structure of pop_list
+str(pop_list)
+
+# Import the first Excel sheet of urbanpop_nonames.xlsx (R gives names): pop_a
+pop_a <- read_excel("urbanpop_nonames.xlsx", col_names = FALSE, sheet = 1)
+
+# Import the first Excel sheet of urbanpop_nonames.xlsx (specify col_names): pop_b
+cols <- c("country", paste0("year_", 1960:1966))
+pop_b <- read_excel("urbanpop_nonames.xlsx", col_names = cols, sheet = 1)
+
+# Print the summary of pop_a
+summary(pop_a)
+
+# Print the summary of pop_b
+summary(pop_b)
+
+# Import the second sheet of urbanpop.xlsx, skipping the first 21 rows: urbanpop_sel
+urbanpop_sel <- read_excel("urbanpop.xlsx", col_names = FALSE, sheet = 2, skip = 21)
+
+# Print out the first observation from urbanpop_sel
+head(urbanpop_sel, n=1)
+urbanpop_sel[1,]
+
+
+
+#CHAPTER4 #chapter 3
+# ------------ 
+
+# CHAPTER 4
+
+
+# gdata
+library(gdata)
+
+# Column names for urban_pop
+columns <- c("country", paste0("year_", 1967:1974))
+
+# Finish the read.xls call
+urban_pop <- read.xls("urbanpop.xls", sheet = 2,
+                      skip = 50, header = TRUE, stringsAsFactors = TRUE,
+                      col.names = columns)
+
+# Print first 10 observation of urban_pop
+head(urban_pop, n=10)
+
+# Add code to import data from all three sheets in urbanpop.xls
+path <- "urbanpop.xls"
+urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
+urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
+urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = FALSE)
+
+# Extend the cbind() call to include urban_sheet3: urban
+urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
+
+# Remove all rows with NAs from urban: urban_clean
+urban_clean <- na.omit(urban)
+
+# Print out a summary of urban_clean
+summary(urban_clean)
+
+# Load the XLConnect package
+
+library(XLConnect)
+
+# Build connection to urbanpop.xlsx: my_book
+# this function will build a "bridge" between your Excel file and your R session.
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Print out the class of my_book
+class(my_book)
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Import columns 3, 4, and 5 from second sheet in my_book: urbanpop_sel
+urbanpop_sel <- readWorksheet(my_book, sheet = 2, startCol = 3, endCol = 5)
+
+# Import first column from second sheet in my_book: countries
+countries <- readWorksheet(my_book, sheet = 2, startCol = 0, endCol = 1)
+
+# cbind() urbanpop_sel and countries together: selection
+selection <- cbind(countries, urbanpop_sel)
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Add a worksheet to my_book, named "data_summary"
+createSheet(my_book, name = "data_summary")
+
+# Use getSheets() on my_book
+getSheets(my_book)
+
+# Build connection to urbanpop.xlsx
+my_book <- loadWorkbook("urbanpop.xlsx")
+
+# Add a worksheet to my_book, named "data_summary"
+createSheet(my_book, "data_summary")
+
+# Create data frame: summ
+sheets <- getSheets(my_book)[1:3]
+dims <- sapply(sheets, function(x) dim(readWorksheet(my_book, sheet = x)), USE.NAMES = FALSE)
+summ <- data.frame(sheets = sheets,
+                   nrows = dims[1, ],
+                   ncols = dims[2, ])
+
+# Add data in summ to "data_summary" sheet
+writeWorksheet(my_book, summ, sheet = "data_summary")
+
+# Save workbook as summary.xlsx
+saveWorkbook(my_book, file= "summary.xlsx")
+
+# Rename "data_summary" sheet to "summary"
+renameSheet(my_book, "data_summary", "summary")
+
+# Print out sheets of my_book
+getSheets(my_book)
+
+# Save workbook to "renamed.xlsx"
+saveWorkbook(my_book, file = "renamed.xlsx")
+
+# Load the XLConnect package
+library(XLConnect)
+
+# Build connection to renamed.xlsx: my_book
+my_book <- loadWorkbook("renamed.xlsx")
+
+# Remove the fourth sheet
+removeSheet(my_book, sheet = "summary")
+
+# Save workbook to "clean.xlsx"
+saveWorkbook(my_book, file = "clean.xlsx")
+
+
+
+
+
