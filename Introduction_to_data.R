8e879940c35159fc19747e1fba38bb86245c595c
gbrlla <gabinp@outlook.com>
2019-03-29 11:14:35 -0300

    introduction to data 1

---
+++ Introduction_to_data.R
@@ -0,0 +1,13 @@
+# COURSE INTRODUCTION TO DATA
+
+install.packages("openintro")
+library(openintro)
+
+
+#Load data
+data(hsb2)
+
+#structure of your data
+#there are two options
+str(hsb2)
+glimpse(hsb2)
